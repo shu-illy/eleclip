1c92072be9a0341fdd7a3564997b55a5b2230565
shu-illy <sh.illy.9117@gmail.com>
2021-03-01 22:53:50 +0900

    Sessions統合テスト生成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン後にユーザー情報のページにリダイレクト
+    else
+      # エラーメッセージを返してnewをレンダリング
+      render 'new'
+    end
+  end
+
+  def destroy
+
   end
 end
--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
 
+  describe "GET login_path" do
+    it "returns success" do
+      get login_path
+      expect(response).to have_http_status(:success)
+    end
+  end
   # describe "GET /new" do
   #   it "returns http success" do
   #     get "/sessions/new"
