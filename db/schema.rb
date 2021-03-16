0d99f243758f44c0d18360495174b336fe4a9e26
shu-illy <sh.illy.9117@gmail.com>
2021-03-16 23:35:59 +0900

    circuitカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -2,17 +2,17 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_134155) do
+ActiveRecord::Schema.define(version: 2021_03_16_142116) do
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
@@ -23,12 +23,13 @@ ActiveRecord::Schema.define(version: 2021_03_07_134155) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  create_table "works", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "works", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "circuit"
     t.index ["user_id", "created_at"], name: "index_works_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_works_on_user_id"
   end
